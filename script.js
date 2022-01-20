216bddbb78d6cb5e95c80dab1e078a05e0967cf8
Clifford <iamclifford321@gmail.com>
2022-01-20 21:32:08 +0800

    all

--- script.js
+++ script.js
@@ -9,10 +9,8 @@
 
   // get page elements
   const video = document.querySelector("#video");
-  const btnPlay = document.querySelector("#btnPlay");
-  const btnPause = document.querySelector("#btnPause");
-  const btnScreenshot = document.querySelector("#btnScreenshot");
-  const btnChangeCamera = document.querySelector("#btnChangeCamera");
+
+
   const screenshotsContainer = document.querySelector("#screenshots");
   const canvas = document.querySelector("#canvas");
   const devicesSelect = document.querySelector("#devicesSelect");
@@ -36,42 +34,8 @@
   // use front face camera
   let useFrontCamera = true;
 
-  // current video stream
   let videoStream;
 
-  // handle events
-  // play
-  btnPlay.addEventListener("click", function () {
-    video.play();
-    btnPlay.classList.add("is-hidden");
-    btnPause.classList.remove("is-hidden");
-  });
-
-  // pause
-  btnPause.addEventListener("click", function () {
-    video.pause();
-    btnPause.classList.add("is-hidden");
-    btnPlay.classList.remove("is-hidden");
-  });
-
-  // take screenshot
-  btnScreenshot.addEventListener("click", function () {
-    const img = document.createElement("img");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    canvas.getContext("2d").drawImage(video, 0, 0);
-    img.src = canvas.toDataURL("image/png");
-    screenshotsContainer.prepend(img);
-  });
-
-  // switch camera
-  btnChangeCamera.addEventListener("click", function () {
-    useFrontCamera = !useFrontCamera;
-
-    initializeCamera();
-  });
-
-  // stop video stream
   function stopVideoStream() {
     if (videoStream) {
       videoStream.getTracks().forEach((track) => {
